037571e9cb906096c4449c5ba7dac8455b22fbf4
Sunny <Sunny@DESKTOP-1NBKD1V>
2020-11-18 11:15:43 +0800

    page change

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    home
+    <el-table :data="tableData">
+      <el-table-column prop="date" label="日期" width="140"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
     <div class="block">
       <el-color-picker v-model="color1"></el-color-picker>
     </div>
@@ -8,11 +12,17 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        color1: '#409EFF'
-      }
-    }
-  };
+export default {
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄",
+    };
+    return {
+      tableData: Array(20).fill(item),
+      color1: "#409EFF",
+    };
+  },
+};
 </script>
--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -1,41 +1,36 @@
 <template>
   <div class="map">
-    <div id="mapdemo"></div>
-    <!-- <baidu-map id="Bmap" @ready="mapReady"> </baidu-map> -->
+    <baidu-map id="Bmap" @ready="mapReady"> </baidu-map>
   </div>
 </template>
 <script>
-import BMap from "vue-baidu-map/components/map/Map";
 export default {
   data() {
     return {
-      //   point: "",
+      point: "",
     };
   },
   methods: {
-    createMap() {
-      //创建Map实例
-      let map = new BMap.Map('map')
-      map.centerAndZoom(new BMap.Point(116.404, 39.915), 12); // 初始化地图,设置中心点坐标和地图级别
+    mapReady({ BMap, map }) {
+      // 选择一个经纬度作为中心点  114.330558,30.601513-武汉
+      this.point = new BMap.Point(116.404, 39.915); // 初始化地图,设置中心点坐标和地图级别
+      map.centerAndZoom(this.point, 12);
+      map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
+      var scaleCtrl = new BMap.ScaleControl(); // 添加比例尺控件
+      map.addControl(scaleCtrl);
+      var navControl = new BMap.NavigationControl(); //添加缩放
+      map.addControl(navControl);
     },
-    // mapReady({ BMap, map }) {
-    //   // 选择一个经纬度作为中心点  114.330558,30.601513-武汉
-    //   this.point = new BMap.Point(116.404, 39.915); // 初始化地图,设置中心点坐标和地图级别
-    //   map.centerAndZoom(this.point, 12);
-    //   map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
-    //   var scaleCtrl = new BMap.ScaleControl(); // 添加比例尺控件
-    //   map.addControl(scaleCtrl);
-    // },
   },
   mounted() {
-    this.createMap();
+    // this.createMap();
   },
 };
 </script>
 
 <style lang="scss" scoped>
 /* 设定地图的大小 */
-.map {
+#Bmap {
   width: auto;
   height: 600px;
   margin: 0 auto;
