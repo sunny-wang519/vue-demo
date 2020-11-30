20d13c161a2bc2928e2f5379126b57859e836e39
muyi <1752993763@qq.com>
2020-11-30 11:40:47 +0800

    添加验证码

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import router from './router'
 import store from './store'
 import BaiduMap from 'vue-baidu-map'
 import './assets/css/reset.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(BaiduMap, {
   ak: "zfbPHKyDM9GhrMPltNZxdyxZ0VAMj0pa"
 })
-
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
